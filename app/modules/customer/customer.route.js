b412bd97be8a7587c26c24a52321ee6eb0442c75
judyjose008 <judy@elaachi.com>
2023-01-13 13:31:33 +0530

    Fetaure for getting a customer done

--- app/modules/customer/customer.route.js
+++ app/modules/customer/customer.route.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const createCustomer = require('./create.controller');
+const getCustomer = require('./get.customer');
 const router = express.Router();
 
 router.post('/customer', (req, res) => { return createCustomer(req, res)});
+router.get('/customer/:id', (req, res) => { return getCustomer(req, res)});
 
 module.exports = router;
